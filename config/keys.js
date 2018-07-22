729382a95b316474506e49bef15911612f048737
Goodnews Ofoegbu <goodmomen@gmail.com>
2018-07-22 15:00:12 +0100

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+   // mongoURI: 'mongodb://<codeme>:<Access2066>@ds143461.mlab.com:43461/socialmediaapp'
+   mongoURI: 'mongodb://localhost:27017/devconnector'
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//Routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+//Routes
+
+const app = express();
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo DB
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log('MongoDB Connected'))
+.catch(error => console.log(error));
+
+app.get('/', (req, res) => res.send('Hello, World'));
+
+
+//Use Route
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+//Use Route
+
+//Deployment setting process.env.PORT allow you to deploy to heroku while 5000 allow you to run it on localhost
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
